691ea97480e4395dac947ac8277ba1c24e895dee
Brian Lauber <constructible.truth@gmail.com>
2015-07-27 15:43:18 -0400

    Code ensures that values are Functions before rebinding them.  Added callback support to 'bound' function.

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
+'use strict';
 
 
-exports.bound = function(obj, funcName) {
-  return obj[funcName].bind(obj);
+exports.bound = function(obj, funcName, cb) {
+  'use strict';
+  var f = obj[funcName];
+
+  if((typeof f) === 'function') {
+    var bound_f = f.bind(obj);
+    if(cb) {
+      cb(bound_f);
+    }
+
+    return bound_f;
+  }
 }
 
 
 exports.rt = function(obj, cb) {
-  var retval = exports.bound.bind(null, obj)
-
-  if(cb) {
-    cb(retval);
+  'use strict';
+  function binder(funcName, cb2) {
+    return exports.bound(binder.this, funcName, cb2);
   }
 
-  return retval;
+  // Expose obj just in case people need to
+  // debug their code.
+  binder.this = obj;
+
+  if(cb) {
+    cb(binder);
+  }
+
+  return binder;
 }
